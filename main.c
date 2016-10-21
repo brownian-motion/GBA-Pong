4705ee35f3e55eb44336b83fdb874a326cec5a4e
Jack Brown <jack.brown.255@gmail.com>
2016-10-20 22:03:17 -0500

    Added pointers to memory-mapped blocks. Added utility functions. Copied pong code.

--- main.c
+++ main.c
@@ -1,19 +1,73 @@
 #include "memory_constants.h"
 
 int main(void) {
-	// Write into the I/O registers, setting video display parameters.
-	volatile unsigned char *ioram = (unsigned char *) MEM_ADDR_IO_RAM;
-	ioram[0] = 0x03; // Set the 'video mode' to 3 (in which BG2 is a 16 bpp bitmap in VRAM)
-	ioram[1] = BG2; // Enable BG2 (BG0 = 1, BG1 = 2, BG2 = 4, ...)
+	// Write the tiles for our sprites into the 4th tile block in VRAM.
+	// Particularly, four tiles for an 8x32 paddle sprite, and 1 tile for an 8x8 ball sprite.
+	// 0x1111 = 0001000100010001 [4bpp = colour index 1, colour index 1, colour index 1, colour index 1]
+	// 0x2222 = 0002000200020002 [4bpp = colour index 2, colour index 2, colour index 2, colour index 2]
+	// NOTE: We're using our own memory writing code here to avoid the byte-granular writes that
+	// something like 'memset' might make (GBA VRAM doesn't support byte-granular writes).
+	volatile uint16 *paddle_tile_memory = (uint16 *)tile_memory[4][1];
+	for (int i = 0; i < 4 * (sizeof(tile4bpp) / 2); ++i) { paddle_tile_memory[i] = 0x1111; }
+	volatile uint16 *ball_tile_memory = (uint16 *)tile_memory[4][5];
+	for (int i = 0; i < (sizeof(tile4bpp) / 2); ++i) { ball_tile_memory[i] = 0x2222; }
 
-	// Write pixel colours into VRAM
-	volatile unsigned short *vram = (unsigned short *) MEM_ADDR_VRAM;
-	vram[xy(115,80)] = rgb(MASK_RED,0,0); // X = 115, Y = 80, C = 000000000011111 = R
-	vram[xy(120,80)] = rgb(0,MASK_RED,0); // X = 120, Y = 80, C = 000001111100000 = G
-	vram[xy(125,80)] = rgb(0,0,MASK_RED); // X = 125, Y = 80, C = 111110000000000 = B
+	// Write the colour palette for our sprites into the first palette of
+	// 16 colours in colour palette memory (this palette has index 0).
+	object_palette_memory[1] = rgb(0x1F, 0x1F, 0x1F); // White
+	object_palette_memory[2] = rgb(0x1F, 0x00, 0x1F); // Magenta
 
-	// Wait forever
-	while(1);
+	// Create our sprites by writing their object attributes into OAM memory.
+	volatile object_attributes *paddle_attributes = &oam_memory[0];
+	paddle_attributes->attribute_zero = 0x8000; // This sprite is made up of 4bpp tiles and has the TALL shape.
+	paddle_attributes->attribute_one = 0x4000; // This sprite has a size of 8x32 when the TALL shape is set.
+	paddle_attributes->attribute_two = 1; // This sprite's base tile is the first tile in tile block 4, and this sprite should use colour palette 0.
+	volatile object_attributes *ball_attributes = &oam_memory[1];
+	ball_attributes->attribute_zero = 0; // This sprite is made up of 4bpp tiles and has the SQUARE shape.
+	ball_attributes->attribute_one = 0; // This sprite has a size of 8x8 when the SQUARE shape is set.
+	ball_attributes->attribute_two = 5; // This sprite's base tile is the fifth tile in tile block 4, and this sprite should use colour palette 0.
+
+	// Initialize our variables to keep track of the state of the paddle and ball,
+	// and set their initial positions (by modifying their attributes in OAM).
+	const int player_width = 8, player_height = 32, ball_width = 8, ball_height = 8;
+	int player_velocity = 2, ball_velocity_x = 2, ball_velocity_y = 1;
+	int player_x = 5, player_y = 96;
+	int ball_x = 22, ball_y = 96;
+	setObjectPosition(paddle_attributes, player_x, player_y);
+	setObjectPosition(ball_attributes, ball_x, ball_y);
+
+	// Set the display parameters to enable objects, and use a 1D object->tile mapping.
+	REG_DISPLAY = 0x1000 | 0x0040;
+
+	// Our main game loop
+	uint32 key_states = 0;
+	while (1) {
+		// Skip past the rest of any current V-Blank, then skip past the V-Draw
+		while(REG_DISPLAY_VCOUNT >= 160);
+		while(REG_DISPLAY_VCOUNT < 160);
+
+		// Get current key states (REG_KEY_INPUT stores the states inverted)
+		key_states = ~REG_KEY_INPUT & KEY_ANY;
+
+		// Note that our physics update is tied to the framerate rather than a fixed timestep.
+		int player_max_clamp_y = SCREEN_HEIGHT - player_height;
+		if (key_states & KEY_UP) { player_y = clamp(player_y - player_velocity, 0, player_max_clamp_y); }
+		if (key_states & KEY_DOWN) { player_y = clamp(player_y + player_velocity, 0, player_max_clamp_y); }
+		if (key_states & (KEY_UP | KEY_DOWN)) { setObjectPosition(paddle_attributes, player_x, player_y); }
+
+		int ball_max_clamp_x = SCREEN_WIDTH - ball_width, ball_max_clamp_y = SCREEN_HEIGHT - ball_height;
+		if ((ball_x >= player_x && ball_x <= player_x + player_width) && (ball_y >= player_y && ball_y <= player_y + player_height)) {
+			// This is not good physics / collision handling code.
+			ball_x = player_x + player_width;
+			ball_velocity_x = -ball_velocity_x;
+		} else {
+			if (ball_x == 0 || ball_x == ball_max_clamp_x) { ball_velocity_x = -ball_velocity_x; }
+			if (ball_y == 0 || ball_y == ball_max_clamp_y) { ball_velocity_y = -ball_velocity_y; }
+		}
+		ball_x = clamp(ball_x + ball_velocity_x, 0, ball_max_clamp_x);
+		ball_y = clamp(ball_y + ball_velocity_y, 0, ball_max_clamp_y);
+		setObjectPosition(ball_attributes, ball_x, ball_y);
+	}
 
 	return 0;
 }
--- memory_constants.h
+++ memory_constants.h
@@ -1,5 +1,19 @@
 #pragma once
 
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef uint16 rgb15;
+typedef uint32 xyCoord;
+typedef struct object_attributes {
+	uint16 attribute_zero;
+	uint16 attribute_one;
+	uint16 attribute_two;
+	uint16 pad;
+} __attribute__((aligned(4))) object_attributes;
+typedef uint32 tile4bpp[8];
+typedef tile4bpp tile_block[512];
+
 #define BG0 1
 #define BG1 2
 #define BG2 4
@@ -7,39 +21,78 @@
 #define BG4 16
 #define BG5 32
 
-#define MEM_ADDR_IO_RAM 0x04000000
-#define MEM_ADDR_VRAM 0x06000000
+#define MEM_ADDR_IO_RAM 		0x04000000
+#define MEM_ADDR_COLOR_PALETTE 	0x05000000
+#define MEM_ADDR_VRAM 			0x06000000
+#define MEM_ADDR_OAM 			0x07000000
 
-#define SCREEN_WIDTH 240
+#define MEM_ADDR_KEYINPUT_IO_REGISTER 	0x04000130
 
-#define MASK_RED 0x001F
-#define MASK_GREEN 0x03E0
-#define MASK_BLUE 0x7C00
 
-inline unsigned short rgb(unsigned short r, unsigned short g, unsigned short b){
-	return (r & MASK_RED) | ((g << 5) & MASK_GREEN) | ((b << 10) & MASK_BLUE);
+#define MEM_ADDR_COLOR_PALETTE_BLOCK_1 	0x5000200
+#define MEM_ADDR_TILE_BLOCK_4 			0x6010000
+#define MEM_ADDR_TILE_BLOCK_5 			0x6014000
+
+#define SCREEN_HEIGHT 	160
+#define SCREEN_WIDTH 	240
+
+#define MASK_RED 	0x001F
+#define MASK_GREEN 	0x03E0
+#define MASK_BLUE 	0x7C00
+
+#define REG_DISPLAY        (*((volatile uint32 *)(MEM_ADDR_IO_RAM)))
+#define REG_DISPLAY_VCOUNT (*((volatile uint32 *)(MEM_ADDR_IO_RAM + 0x0006)))  //Count of lines drawn. This varies from 0 to 227. If >= SCREEN_HEIGHT, then we're in a VBLANK
+#define REG_KEY_INPUT      (*((volatile uint32 *)(MEM_ADDR_IO_RAM + 0x0130)))
+
+#define KEY_UP     0x0040
+#define KEY_DOWN   0x0080
+#define KEY_ANY    0x03FF
+
+#define OBJECT_ATTRIBUTE_ZERO_Y_MASK  0xFF
+#define OBJECT_ATTRIBUTE_ONE_X_MASK  0x1FF
+
+#define oam_memory ((volatile object_attributes *)MEM_ADDR_OAM)
+#define tile_memory ((volatile tile_block *)MEM_ADDR_VRAM)
+#define object_palette_memory ((volatile rgb15 *)(MEM_ADDR_COLOR_PALETTE + 0x200))
+
+static inline rgb15 rgb(unsigned short r, unsigned short g, unsigned short b){
+	return r | (g << 5) | (b << 10);
 }
 
-inline unsigned short red(unsigned short color){
+static inline unsigned short red(rgb15 color){
 	return color & MASK_RED;
 }
 
-inline unsigned short green(unsigned short color){
+static inline unsigned short green(rgb15 color){
 	return (color & MASK_GREEN) >> 5;
 }
 
-inline unsigned short blue(unsigned short color){
+static inline unsigned short blue(rgb15 color){
 	return (color & MASK_BLUE) >> 10;
 }
 
-inline int xy(unsigned int x, unsigned int y){
+static inline xyCoord xy(unsigned int x, unsigned int y){
 	return SCREEN_WIDTH*y + x;
 }
 
-inline int getXFromCoordinate(unsigned int coordinate){
+static inline int getXFromCoordinate(xyCoord coordinate){
 	return coordinate % SCREEN_WIDTH;
 }
 
-inline int getYFromCoordinate(unsigned int coordinate){
+static inline int getYFromCoordinate(xyCoord coordinate){
 	return coordinate / SCREEN_WIDTH;
+}
+
+static inline int clamp(int value, int min, int max){
+	return (value > max) ? max:((value < min) ? min : value);
+}
+
+static inline void setObjectPosition(volatile object_attributes* object, int x, int y){
+	object->attribute_zero =
+		(object->attribute_zero & ~OBJECT_ATTRIBUTE_ZERO_Y_MASK)
+		| (y & OBJECT_ATTRIBUTE_ZERO_Y_MASK);
+
+	object->attribute_one =
+		(object->attribute_one & ~OBJECT_ATTRIBUTE_ONE_X_MASK)
+		| (x & OBJECT_ATTRIBUTE_ONE_X_MASK);
 }
